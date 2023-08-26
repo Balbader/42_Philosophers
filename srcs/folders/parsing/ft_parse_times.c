30e8850dee100d8d110d0075c95c3c19452f6edf
balbader <basilalbapro@gmail.com>
2023-08-26 17:14:12 +0200

    ft_parse_times.c

---
+++ srcs/folders/parsing/ft_parse_times.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_times.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 17:13:20 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/26 17:13:30 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*
+ * file		:	parsing.c
+ * function	:	t_bool record_times
+*/
+
+t_bool	ft_parse_times(char **av, t_config *config)
+{
+	static int	i = 2;
+	char		*str;
+	long		time;	
+
+	str = av[i] + (*av[i] == '+');
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (FALSE);
+		str++;
+	}
+	time = ft_atoi_long(argv[i]);
+	if (time > INT_MAX)
+		return (FALSE);
+	if (i == 2)
+		config->time_to_die = time;
+	else if (i == 3)
+		config->time_to_eat = time;
+	else
+		config->time_to_sleep = time;
+	if (i++ < 4)
+		return (ft_parse_times(av, config));
+	return (TRUE);
+}

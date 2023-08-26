f1a06c5d1385797d7cdc97b26f3269418795e97e
balbader <basilalbapro@gmail.com>
2023-08-26 09:35:25 +0200

    define.h

---
+++ inc/define.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 09:30:06 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/26 09:30:07 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_H
+# define DEFINE_H
+
+# define FORK			"has taken a fork"
+# define EAT			"is eating"
+# define SLEEP			"is sleeping"
+# define THINK			"is thinking"
+# define DIE			"died"
+# define MIN_NB_MEAL	"Each philosopher has eaten at least"
+
+#endif // !DEFINE_H

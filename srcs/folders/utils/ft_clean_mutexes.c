3b69df3b6b7da0872055eee648a201ea3c5c8d9a
balbader <basilalbapro@gmail.com>
2023-08-26 18:20:46 +0200

    ft_clean_mutexes.c

---
+++ srcs/folders/utils/ft_clean_mutexes.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_clean_mutexes.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 18:14:22 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/26 18:14:24 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_clean_mutexes(t_mutexes *mutexes, int nb_philo)
+{
+	int		i;
+
+	if (pthread_mutex_destroy(&mutexes->is_speaking) != 0)
+		ft_print_err(MTX_DESTROY, NULL, ERROR);
+	i = 0;
+	while (i < nb_philo)
+	{
+		if (pthread_mutex_destroy(&mutexes->forks[i]) != 0)
+			ft_print_err(MTX_DESTROY, NULL, ERROR);
+		if (pthread_mutex_destroy(&mutexes->is_eating[i++]) != 0)
+			ft_print_err(MTX_DESTROY, NULL, ERROR);
+	}
+	free(mutexes->forks);
+	free(mutexes->is_eating);
+}

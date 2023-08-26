8713082492cdc694eccd393b1243a1c93b158455
balbader <basilalbapro@gmail.com>
2023-08-26 16:32:00 +0200

    ft_check_meal_count.c

---
+++ srcs/folders/check_dinner/ft_check_meal_count.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_meal_count.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 16:24:00 by baalbade          #+#    #+#             */
+/*   Updated: 2023/08/26 16:24:05 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*
+ * file		:	monitor_diner.c
+ * function	:	void monitor_nb_meals
+*/
+
+void	ft_check_meal_count(t_philo *philosophers, int nb_philo,
+		int min_nb_meal)
+{
+	int			i;
+	t_philo		*philo;
+
+	while (!*philosophers[0].is_diner_over)
+	{
+		i = 0;
+		while (i < nb_philo)
+		{
+			philo = &philosophers[i];
+			if (philo->nb_meal < min_nb_meal)
+				break ;
+			if ((i + 1) == nb_philo)
+			{
+				speak(philo, get_time(), MIN_NB_MEAL);
+				*philo->is_diner_over = TRUE;
+				return ;
+			}
+			i++;
+		}
+		ft_usleep(1000);
+	}
+}

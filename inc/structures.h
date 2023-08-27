c1759a2d06db9f3576a454456ae842c4b9a467ac
balbader <basilalbapro@gmail.com>
2023-08-27 18:45:25 +0200

    WIP

--- inc/structures.h
+++ inc/structures.h
@@ -23,45 +23,45 @@ typedef enum e_bool
 
 typedef struct s_mutexes
 {
-	pthread_mutex_t	is_speaking;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	*is_eating;
+	pthread_mutex_t		is_speaking;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		*is_eating;
 }				t_mutexes;
 
 typedef struct s_diner
 {
-	t_mutexes	mutexes;
-	pthread_t	*philo_threads;
-	pthread_t	*philo_monitors;
-	t_bool		is_diner_over;
+	t_mutexes			mutexes;
+	pthread_t			*philo_threads;
+	pthread_t			*philo_monitors;
+	t_bool				is_diner_over;
 }				t_diner;
 
 typedef struct s_config
 {
-	long	nb_philo;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	long	min_nb_meal;
+	long				nb_philo;
+	long				time_to_die;
+	long				time_to_eat;
+	long				time_to_sleep;
+	long				min_nb_meal;
 }				t_config;
 
 typedef struct s_philo
 {
-	int				id;
-	int				nb_meal;
-	int				min_nb_meal;
-	time_t			last_meal;
-	time_t			start_time;
-	time_t			time_to_die;
-	time_t			time_to_eat;
-	time_t			time_to_sleep;
-	pthread_mutex_t	*is_thinking;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	*is_eating;
-	int				nb_forks;
-	int				fork_left;
-	int				fork_right;
-	t_bool			*is_diner_over;
+	int					id;
+	int					nb_meal;
+	int					min_nb_meal;
+	int					nb_forks;
+	int					fork_left;
+	int					fork_right;
+	time_t				last_meal;
+	time_t				start_time;
+	time_t				time_to_die;
+	time_t				time_to_eat;
+	time_t				time_to_sleep;
+	t_bool				*is_diner_over;
+	pthread_mutex_t		*is_thinking;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		*is_eating;
 }				t_philo;
 
 #endif

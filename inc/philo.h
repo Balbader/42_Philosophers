340099be7eebb0c1a76c69a422766d746a231164
balbader <basilalbapro@gmail.com>
2023-08-27 18:21:39 +0200

    .h

--- inc/philo.h
+++ inc/philo.h
@@ -32,22 +32,22 @@ t_bool	ft_record_times(char **av, t_config *config);
 
 // Init
 t_bool	ft_init_mutexes_arr(pthread_mutex_t *forks, pthread_mutex_t *is_eating,
-		int nb_philo);
+			int nb_philo);
 t_bool	ft_init_mutexes(t_mutexes *mutexes, int nb_philo);
 t_bool	ft_init_philosophers(t_philo **philosophers, int nb_philo,
-		t_diner *diner_setup, t_config *config);
+			t_diner *diner_setup, t_config *config);
 void	ft_setup_philosophers(t_philo *philo, int i, t_diner *diner_setup,
-		t_config *config);
+			t_config *config);
 
 // Start Dinner
 t_bool	ft_init_threads_monitoring(pthread_t *philo_threads,
 			pthread_t *philo_monitor, t_philo *philosophers, int i);
 t_bool	ft_init_threads(pthread_t *philo_threads, pthread_t *philo_monitors,
-		t_philo *philosophers, int i);
+			t_philo *philosophers, int i);
 t_bool	ft_start_threads(t_diner *diner_setup, t_philo *phlosophers,
-		int nb_philo);
+			int nb_philo);
 t_bool	ft_start_dinner(t_diner *diner_setup, t_philo *philosophers,
-		int nb_philo);
+			int nb_philo);
 
 // Dinner
 t_bool	ft_eat(t_philo *philo);
@@ -56,7 +56,7 @@ void	*ft_diner(void *arg);
 
 // Check Dinner
 void	ft_check_meal_count(t_philo *philosophers, int nb_philo,
-		int min_nb_meal);
+			int min_nb_meal);
 void	*ft_check_philosopher(void *arg);
 
 // Think

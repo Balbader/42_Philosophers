f11118800a50e13c035131cc7b3cd97c844f8097
Basil ALBADER <baalbade@student.42.fr>
2023-09-02 09:47:53 +0200

    ft_is_eating.c

---
+++ srcs/dinner/ft_is_eating.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_eating.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 09:44:45 by baalbade          #+#    #+#             */
+/*   Updated: 2023/09/02 09:44:46 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/philo.h"
+
+void	ft_is_eating(t_rules *r, t_philo *p)
+{
+	pthread_mutex_lock(&(r->health_check));
+	p->last_meal = ft_get_time();
+	p->nb_meal++;
+	pthread_mutex_unlock(&(r->health_check));
+	print_action(p, r, "is eating");
+	ft_usleep(r->time_to_eat);
+}

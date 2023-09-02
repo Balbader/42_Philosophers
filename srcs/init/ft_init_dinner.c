b535a6cf29f9232f52deaff470183c5908fac116
Basil ALBADER <baalbade@student.42.fr>
2023-09-02 09:07:11 +0200

    ft_init_dinner.c

---
+++ srcs/init/ft_init_dinner.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_dinner.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 09:06:12 by baalbade          #+#    #+#             */
+/*   Updated: 2023/09/02 09:06:14 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/philo.h"
+
+int	ft_init_dinner(t_rules *rules, char **av)
+{
+	rules->nb_philo = ft_atoi(av[1]);
+	rules->time_to_die = ft_atoi(av[2]);
+	rules->time_to_eat = ft_atoi(av[3]);
+	rules->time_to_sleep = ft_atoi(av[4]);
+	rules->dead_end = 0;
+	rules->all_ate = 0;
+	if (rules->nb_philo < 1 || rules->nb_philo > 200 || rules->time_to_die < 1
+		|| rules->time_to_eat < 1 || rules->time_to_sleep < 1)
+		return (write(2, "Error:\nSome args aren't valid\n", 31));
+	if (av[5])
+	{
+		rules->x_meal = ft_atoi(av[5]);
+		if (rules->x_meal <= 0)
+			return (write(2, "Error:\nPhilos have to eat once at least.\n", 41));
+	}
+	else
+		rules->x_meal = -1;
+	if (ft_init_mutex(rules))
+		return (1);
+	if (ft_init_philosophers(rules))
+		return (1);
+	return (0);
+}

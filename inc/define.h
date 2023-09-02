cb86f3c2c58baeaacbcbd14f3e973897cf953d26
Basil ALBADER <baalbade@student.42.fr>
2023-09-02 11:13:44 +0200

    .h

--- inc/define.h
+++ inc/define.h
@@ -13,11 +13,16 @@
 #ifndef DEFINE_H
 # define DEFINE_H
 
-# define EATING		"is eating"
-# define SLEEPING	"is sleeping"
-# define THINKING	"is thinking"
-# define DIED		"died"
-# define FORK		"has taken a fork"
-# define ARG_ERR	"Error! Non valid arguments."
+# define EATING				"is eating"
+# define SLEEPING			"is sleeping"
+# define THINKING			"is thinking"
+# define DIED				"died"
+# define FORK				"has taken a fork"
+# define ARG_ERR			"Error! Non valid arguments."
+# define ARG_NB_ERR			"Error! Non valid arguments count."
+# define ARG_LIMIT_ERR		"Error! Value is out of range."
+# define MEAL_ERR			"Error! All philosophers have to eat at least once."
+# define MUTEX_ERR			"Error! An issue occured while creating a mutex."
+# define THREAD_ERR			"Error! An issue occured while creating a thread."
 
 #endif // !DEFINE_H

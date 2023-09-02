53826dae044303a40571ba622b2466ecca6d138f
Basil ALBADER <baalbade@student.42.fr>
2023-09-02 10:34:12 +0200

    WIP

--- srcs/dinner/ft_threader.c
+++ srcs/dinner/ft_threader.c
@@ -20,18 +20,18 @@ void	*ft_threader(void *void_philo)
 	p = (t_philo *)void_philo;
 	r = p->rules;
 	if (r->nb_philo == 1)
-		return (solo_philo(r, p));
+		return (ft_philo_alone(r, p));
 	if (p->id % 2 == 0)
 		ft_usleep(r->time_to_eat);
 	while (1)
 	{
-		if (check_health(r) == 0)
+		if (ft_check_health(r) == 0)
 			break ;
 		pthread_mutex_lock(&(r->forks[p->right_fork_idx]));
 		ft_print_state(p, r, "has taken a fork");
 		pthread_mutex_lock(&(r->forks[p->left_fork_idx]));
 		ft_print_state(p, r, "has taken a fork");
-		handle_eat(r, p);
+		ft_is_eating(r, p);
 		pthread_mutex_unlock(&(r->forks[p->right_fork_idx]));
 		pthread_mutex_unlock(&(r->forks[p->left_fork_idx]));
 		ft_print_state(p, r, "is sleeping");

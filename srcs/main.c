942a297b81efbcf1f8cdea39dafddd269fb86915
Basil ALBADER <baalbade@student.42.fr>
2023-09-02 09:16:01 +0200

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/philo.h"
 
+int	main(int ac, char **av)
+{
+	t_rules			*rules;
+
+	rules = malloc(sizeof(t_rules) * 1);
+	if (!rules)
+		return (write(2, "Error:\nMalloc issue\n", 21));
+	if (ft_check_args(ac, av))
+	{
+		free(rules);
+		return (0);
+	}
+	if (ft_init_dinner(rules, av) > 1)
+	{
+		free(rules);
+		return (0);
+	}
+	else if (ft_init_dinner(rules, av) == 1)
+		return (ft_destroy_mutexes(rules));
+	if (launch_sim(rules))
+		return (ft_destroy_mutexes(rules));
+	return (1);
+}

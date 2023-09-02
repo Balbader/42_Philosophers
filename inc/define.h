f00bf0a099a51e683867d6bc2dfe7e249c3d98db
Basil ALBADER <baalbade@student.42.fr>
2023-09-02 10:57:46 +0200

    WIP

---
+++ inc/define.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 10:47:54 by baalbade          #+#    #+#             */
+/*   Updated: 2023/09/02 10:47:56 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_H
+# define DEFINE_H
+
+# define EATING		"is eating"
+# define SLEEPING	"is sleeping"
+# define THINKING	"is thinking"
+# define DIED		"died"
+# define FORK		"has taken a fork"
+# define ARG_ERR	"Error! Non valid arguments."
+
+#endif // !DEFINE_H

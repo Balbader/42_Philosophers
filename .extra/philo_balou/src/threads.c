48141840a3761a5c1c48afb1f4e46a40ea2117f4
Basil ALBADER <baalbade@student.42.fr>
2023-09-07 08:28:28 +0200

    extra

---
+++ .extra/philo_balou/src/threads.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kharfach <kharfach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 14:06:20 by kharfach          #+#    #+#             */
+/*   Updated: 2023/05/01 11:25:49 by kharfach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+void	*thread(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->number % 2 == 0)
+		usleep(1000);
+	while (is_dead(philo) != DEATH && philo->data->meals[philo->number - 1] > 0)
+	{	
+		pthread_mutex_lock(&philo->data->print_mutex);
+		pthread_mutex_unlock(&philo->data->print_mutex);
+		if (eat(&philo) == DEATH || mysleep(philo) == DEATH)
+			return (NULL);
+	}
+	return (NULL);
+}
